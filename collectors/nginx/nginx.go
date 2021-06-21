178debba6018e5f1e52abe90a45e5e66335f3d88
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-06-21 06:09:57 +0600

    wip dumping transport layer stuff

--- collectors/nginx/nginx.go
+++ collectors/nginx/nginx.go
@@ -113,15 +113,15 @@ func main()  {
 		// Decode all layers and return them.  The layers up to the first IPv4 layer
 		// are already decoded, and will not require decoding a second time.
 
-		if app := packet.ApplicationLayer(); app != nil {
+		if transport := packet.TransportLayer(); transport != nil {
 
 			tcp := packet.TransportLayer().(*layers.TCP)
 
 			assembler.AssembleWithTimestamp(packet.NetworkLayer().NetworkFlow(), tcp, packet.Metadata().Timestamp)
 
-			fmt.Printf("application layer packet payload : %v\n application layer LayerPayload %v\n",
-				string(app.Payload()),
-				string(app.LayerPayload()),
+			fmt.Printf("transportlication layer packet payload : %v\n transportlication layer LayerPayload %v\n",
+				string(transport.LayerContents()),
+				string(transport.LayerPayload()),
 			)
 		}
 
